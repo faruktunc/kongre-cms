71847a1725aed158b9d716e2e46945fc0beb6885
faruktunc <omerfaruktunc407@gmail.com>
2026-06-21 16:26:52 +0300

    site verification

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="google-site-verification" content="6Fruqz4ifl1qSiyzd46V3aaUkAJh8uCluZVnLAHwhZ0">
+    <meta name="google-site-verification" content="wASHq_ZuQPsc23vyr6BFoK9R-zMq_E4QkFTmCQHZNOs" />
     @viteReactRefresh
     @vite('resources/js/app.jsx')
     @vite('resources/css/app.css')
